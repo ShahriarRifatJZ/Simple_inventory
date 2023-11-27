c6b4288ad16511a33d4e96590df59fdc9d100dbf
Rifat07 <shahriar7rifat@gmail.com>
2023-11-27 17:28:31 +0600

    inventory

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  signUpButton: HTMLElement | null;
+  signInButton: HTMLElement | null;
+  container: HTMLElement | null;
 
-  ngOnInit(): void {
+  constructor() {
+    this.signUpButton = null;
+    this.signInButton = null;
+    this.container = null;
   }
 
+  ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.signUpButton = document.getElementById('signUp');
+      this.signInButton = document.getElementById('signIn');
+      this.container = document.getElementById('container');
+
+      if (this.signUpButton && this.signInButton && this.container) {
+        this.signUpButton.addEventListener('click', () => {
+          this.container!.classList.add("right-panel-active");
+        });
+
+        this.signInButton.addEventListener('click', () => {
+          this.container!.classList.remove("right-panel-active");
+        });
+      }
+    });
+  }
 }
